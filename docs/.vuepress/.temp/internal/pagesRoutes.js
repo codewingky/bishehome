ebc9b57c165a4c2c067fe21bf85c0db9fe4ddb1b
y z <2427644955@qq.com>
2023-02-26 22:17:42 +0800

    修复提交

--- docs/.vuepress/.temp/internal/pagesRoutes.js
+++
@@ -1,5 +0,0 @@
-export const pagesRoutes = [
-  ["v-8daa1a0e","/",{"title":"可视化数据分析平台"},["/index.html","/index.md"]],
-  ["v-fffb8e28","/guide/",{"title":"介绍"},["/guide/index.html","/guide/index.md"]],
-  ["v-3706649a","/404.html",{"title":""},["/404"]],
-]
